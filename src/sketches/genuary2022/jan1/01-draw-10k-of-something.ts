c68253035a44dd54abc7158e9c4578439b73ef37
Donnie Flood <donnie@floodfx.com>
2022-01-01 14:11:00 -0700

    genuary jan1 quick sketch

---
+++ src/sketches/genuary2022/jan1/01-draw-10k-of-something.ts
@@ -0,0 +1,30 @@
+import p5 from "p5";
+// https://genuary.art/prompts#jan1
+function sketch(p: p5) {
+  const WIDTH = p.windowWidth;
+  const HEIGHT = p.windowHeight;
+  const x = 100;
+  const y = 100;
+
+  p.setup = () => {
+    p.createCanvas(WIDTH, HEIGHT);
+    p.colorMode("hsb", 360, 100, 100);
+    p.background(0, 0, 100);
+    
+    p.strokeWeight(1);
+    
+    for(let i = 0; i < 10000; i++) {
+      p.fill(0,0,0,0);
+      p.stroke(0, 0, 0, 0.1);
+      p.triangle(p.random(WIDTH), p.random(HEIGHT), p.random(WIDTH), p.random(HEIGHT), p.random(WIDTH), p.random(HEIGHT));
+    } 
+  };
+
+  p.draw = () => {
+    
+
+  
+  };
+}
+
+new p5(sketch);
